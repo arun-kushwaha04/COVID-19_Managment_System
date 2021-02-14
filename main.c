6fca6806d83ef8f3f83ad196a7697e92e505e16f
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-02-14 21:57:05 +0530

    Database created, Automatic generation and updation of id

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ void addrecord();
 void listrecord();
 void editrecord();
 void deleterecord();
+void show_vacant_beds();
+void get_id();
 void _e_xit();
 
 int main(){
@@ -115,29 +117,33 @@ void main_menu(){
         title();
         mainmenu_heading();
         
-        give_tab();printf("1. Add Patient Record\n\n"); 
-        give_tab();printf("2. List Patient Record\n\n"); 
-        give_tab();printf("3. Edit Patient Record\n\n"); 
-        give_tab();printf("4. Delete Patient Record\n\n");
-        give_tab();printf("5. Exit\n\n"); 
-        give_tab();printf("Enter a number between 1-5\n\n");
+        give_tab();printf("1. Display Vacants Beds\n\n"); 
+        give_tab();printf("2. Add Patient Record\n\n"); 
+        give_tab();printf("3. List Patient Record\n\n"); 
+        give_tab();printf("4. Edit Patient Record\n\n"); 
+        give_tab();printf("5. Delete Patient Record\n\n");
+        give_tab();printf("6. Exit\n\n"); 
+        give_tab();printf("Enter a number between 1-6\n\n");
         give_tab();printf("Enter your choice: ");
         int n;
         scanf("%d", &n);
         switch(n){
             case 1:
-            addrecord();
+            show_vacant_beds();
             break;
             case 2:
-            listrecord();
+            addrecord();
             break;
             case 3:
+            listrecord();
+            break;
+            case 4:
             editrecord();
             break;
-            case 4: 
+            case 5: 
             deleterecord(); 
             break;
-            case 5:
+            case 6:
             _e_xit();
             return;
             break;
@@ -151,7 +157,7 @@ void main_menu(){
 //exit function
 void _e_xit(){
     system("cls");  
-    title();printf("\t\t\t\t\t\t\t\t\t\t         Main Menu\n");
+    title();
     give_tab();printf("This code is written and maintained by---\n");
     give_tab();printf("1. Ansh IMT-014\n");
     give_tab();printf("2. Ketan IMT-044\n");
@@ -168,39 +174,176 @@ void checker(char str[]){
 void addrecord(){
     system("cls");
     title();printf("\t\t\t\t\t\t\t\t\t!!!!!!!!!!!!!!! ADD PATIENT RECORD !!!!!!!!!!!!!!!\n\n\n");
-    FILE *ptr;
-    ptr = fopen("patientdata.csv","a");
-    if(ptr==NULL){
+    FILE *ptr,*ptr2;
+    int n;
+    give_tab();printf("1. AIIMS\n\n"); 
+    give_tab();printf("2. Apollo\n\n"); 
+    give_tab();printf("3. Fortis\n\n"); 
+    give_tab();printf("4. Max Super\n\n"); 
+    give_tab();printf("5. Vedanta\n\n");
+    give_tab();printf("Enter a number between 1-5\n\n");
+    give_tab();printf("Enter your choice: ");
+    scanf("%d", &n);
+    switch(n){
+        case 1:
+        ptr = fopen("./Database/AIIMS/patient.csv","a+");
+        if(ptr==NULL){
         perror("Unable to open file");
         return ;
+        }
+        break;
+        case 2:
+        ptr = fopen("./Database/Apollo/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 3:
+        ptr = fopen("./Database/Fortis/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 4:
+        ptr = fopen("./Database/Max Super/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 5: 
+        ptr = fopen("./Database/Vedanta/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        default: 
+        give_tab();printf("Invalid entry, press enter to try again\n"); getch();
+        break;
     }
-    char a[6],b[4],c[11],d[11],e[4],f,g[11];
+
+    char b[4],c[11],d[11],e[4],f,g[11];
+    get_id(n,b);
     fflush(stdin);
-    give_tab();printf("Enter patient's I'D: ");scanf("%s",a);fflush(stdin);
-    give_tab();printf("Enter patient's Bed number: ");scanf("%s",b);fflush(stdin);
+    printf("\n\n");
+    give_tab();printf("Generated I'd and Bed Number are %s,%s\n",b,b);fflush(stdin);
     give_tab();printf("Enter patient's First name: ");scanf("%s",c);fflush(stdin);
     give_tab();printf("Enter patient's Last name: ");scanf("%s",d);fflush(stdin);
     give_tab();printf("Enter patient's Age: ");scanf("%s",e);fflush(stdin);
     give_tab();printf("Enter patient's gender: ");scanf("%c",&f);fflush(stdin);
     give_tab();printf("Enter patient's Number: ");scanf("%s",g);fflush(stdin);
-    fprintf(ptr,"%s,%3s,%10s,%10s,%3s,%c,%10s\n",a,b,c,d,e,f,g);
+    fprintf(ptr,"%s,%s,%s,%s,%s,%c,%s\n",b,b,c,d,e,f,g);
     fclose(ptr);
     give_tab();printf("Record Created Successfully!!\n\n");
     char choice;
     give_tab();printf("Want to add another record[Y/N]");scanf("%c",&choice);
     if(choice=='Y')addrecord();
-    return;
+    else main_menu();
 }
-void listrecord(){
-    system("cls");
-    title();printf("\t\t\t\t\t\t\t\t!!!!!!!!!!!!!!! PATIENT RECORD !!!!!!!!!!!!!!!\n");
-    FILE *ptr;
-    ptr = fopen("patientdata.csv","r");
+
+//Generating Id and bed number
+void get_id(int n,char b[4]){
+    FILE *ptr = fopen("./Database/Vacant.csv","a+"), *ptr2 = fopen("./Database/temp.csv","a+");
     if(ptr==NULL){
         perror("Unable to open file");
         return ;
     }
+    char info[50];
+    int i=1;
+    
+    while(fgets(info,sizeof(info),ptr)){
+        char *token,*name,*seats,*id;
+        token = strtok(info, ",");
+        int j=1;
+        while( token != NULL ) {
+            if(i==n+1 && j==3){
+                int c;  
+                c=atoi(token);c=c+1;
+                sprintf(b, "%d", c);
+                token = strtok(NULL, ",");
+            }
+            else if(j==1){
+                name = token;
+                token = strtok(NULL, ",");
+            }
+            else if(j==2){
+                seats = token;
+                token = strtok(NULL, ",");
+            }
+            else if(j==3){
+                id = token;
+                token = strtok(NULL, ",");
+            }
+            j++;
+        }
+        if(i==n+1){fprintf(ptr2,"%s,%s,%s\n",name,seats,b);}
+        else {fprintf(ptr2,"%s,%s,%s",name,seats,id);}
+        i++;
+    }
+    fclose(ptr);
+    fclose(ptr2);
+    remove("./Database/Vacant.csv");
+    rename("./Database/temp.csv","./Database/Vacant.csv");
+}
 
+// Listing patient record
+void listrecord(){
+    system("cls");
+    title();printf("\t\t\t\t\t\t\t\t!!!!!!!!!!!!!!! PATIENT RECORD !!!!!!!!!!!!!!!\n");
+    FILE *ptr;
+    int n;
+    give_tab();printf("1. AIIMS\n\n"); 
+    give_tab();printf("2. Apollo\n\n"); 
+    give_tab();printf("3. Fortis\n\n"); 
+    give_tab();printf("4. Max Super\n\n"); 
+    give_tab();printf("5. Vedanta\n\n");
+    give_tab();printf("Enter a number between 1-5\n\n");
+    give_tab();printf("Enter your choice: ");
+    scanf("%d", &n);
+    switch(n){
+        case 1:
+        ptr = fopen("./Database/AIIMS/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 2:
+        ptr = fopen("./Database/Apollo/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 3:
+        ptr = fopen("./Database/Fortis/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 4:
+        ptr = fopen("./Database/Max Super/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        case 5: 
+        ptr = fopen("./Database/Vedanta/patient.csv","a+");
+        if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+        }
+        break;
+        default: 
+        give_tab();printf("Invalid entry, press enter to try again\n"); getch();
+        break;
+    }
+        printf("\n\n");
     char info[100];
     while(fgets(info,sizeof(info),ptr)){
         char *token;
@@ -216,8 +359,40 @@ void listrecord(){
     printf("\n");
     give_tab();printf("This is the end of record");
     getch();
-    return;
+    main_menu();
 }
+
+/*different hospital data*/
+void show_vacant_beds(){
+    system("cls");
+    title();
+    printf("\t\t\t\t\t\t\t\t\t\t---------------------------\n");
+    printf("\t\t\t\t\t\t\t\t\t\t         Vacant Beds\n");
+    printf("\t\t\t\t\t\t\t\t\t\t---------------------------\n\n\n\n");
+    FILE *ptr;
+    ptr = fopen("./Database/Vacant.csv","r");
+    if(ptr==NULL){
+        perror("Unable to open file");
+        return ;
+    }  
+    char info[50];
+    while(fgets(info,sizeof(info),ptr)){
+        char *token;
+        token = strtok(info, ",");
+        while( token != NULL ) {
+            printf("\t\t\t");
+            printf( " %-10s", token );
+            token = strtok(NULL, ",");
+        }
+        printf("\n");
+    }
+    fclose(ptr);
+    printf("\n");
+    give_tab();printf("This is the end of record");
+    getch();
+    main_menu();
+}
+
 void editrecord(){
     system("cls");
     title();printf("\t\t\t\t\t\t\t\t!!!!!!!!!!!!!!! EDIT PATIENT RECORD !!!!!!!!!!!!!!!\n");
